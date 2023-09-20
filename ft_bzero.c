82a3b5d264950555c087b8ddd0bf5d500f3735df
Leegon8 <126713351+Leegon8@users.noreply.github.com>
2023-09-20 21:31:57 +0200

    Add files via upload

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,15 +1,15 @@
  
- //#include "libft.h"
- #include <strings.h>
+//#include "libft.h"
+#include <strings.h>
 
 void    ft_bzero(void *s, size_t n)
 {
-    unsigned char   str;
-    int c;
+  size_t  i;
 
-    str = (unsigned char *)s;
-    c = 0;
-    while (n--)
-		*(unsigned char *)s++ = (unsigned char)c;
-	return (str);
+  i = 0;
+  while (i < n)
+  {
+    ((char *)s)[i] = 0;
+    i++;
+  }
 }
